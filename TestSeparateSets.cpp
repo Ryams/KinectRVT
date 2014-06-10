d2475a0d729f3641fb1f05d265b987d63b77fcfb
Ryan <staabr@gmail.com>
2014-06-09 18:24:54 -0700

    option for which model

--- TestSeparateSets.cpp
+++ TestSeparateSets.cpp
@@ -21,29 +21,25 @@ using namespace std;
 // Enter 'r' for dirChar to get RyanData, anything else to get ThesisData
 void getSamples(char dirChar, const String exerciseTypeName, Mat &m1, Mat &m2, Mat &m3, Mat &labels) {
 	String _mainDirName;
-	String _dirModifier;
 
 	if (dirChar == 'r') {
 		_mainDirName = "RyanData";
-		_dirModifier = "";
 	}
 	else {
 		_mainDirName = "ThesisData";
-		_dirModifier = " CSVs";
 	}
 
 	//TODO: I had to hack this because there was a weird bug where
 	// _exerciseTypeName was getting written over after calling FileStorage fs;
 	const String mainDirName = String(_mainDirName);
-	const String dirModifier = String(_dirModifier);
 
-	FileStorage fs("D:\\" + mainDirName + "\\" + exerciseTypeName + dirModifier + "\\time_samples.xml", FileStorage::READ);
+	FileStorage fs("D:\\" + mainDirName + "\\" + exerciseTypeName + "\\time_samples.xml", FileStorage::READ);
 	fs.getFirstTopLevelNode() >> m1;
-	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + dirModifier + "\\pos_samples.xml", FileStorage::READ);
+	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + "\\pos_samples.xml", FileStorage::READ);
 	fs.getFirstTopLevelNode() >> m2;
-	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + dirModifier + "\\ori_samples.xml", FileStorage::READ);
+	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + "\\ori_samples.xml", FileStorage::READ);
 	fs.getFirstTopLevelNode() >> m3;
-	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + dirModifier + "\\all_labels.xml", FileStorage::READ);
+	fs.open("D:\\" + mainDirName + "\\" + exerciseTypeName + "\\all_labels.xml", FileStorage::READ);
 	fs.getFirstTopLevelNode() >> labels;
 	fs.release();
 }
@@ -60,17 +56,18 @@ void getRawPosFeature(Mat &features, const Mat &timeMat, const Mat &posMat, cons
 
 // TODO: make sure taking the vector difference actually makes sense for quaternion data
 void getOriVelocityFeature(Mat &features, const Mat &timeMat, const Mat &posMat, const Mat &oriMat) {
-	features.create(oriMat.size().height - 1, oriMat.size().width, CV_32F); // Note height is -1 because interaction between rows.
-	for (int i = 0; i < features.size().height; ++i) {
+	//features.create(oriMat.size().height, oriMat.size().width, CV_32F);
+	features = Mat::zeros(oriMat.size().height, oriMat.size().width, CV_32F);
+	for (int i = 1; i < features.size().height - 1; ++i) { // Note height is -1 because interaction between rows.
 		features.row(i) = ((oriMat.row(i + 1) - oriMat.row(i)));// / (timeMat.at<float>(i + 1, 3) - timeMat.at<float>(i, 3)));
 	}
-	cout << "features: " << endl;
+	/*cout << "features: " << endl;
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < features.size().width; ++j) {
 			cout << features.at<float>(i, j) << " ";
 		}
 		cout << endl;
-	}
+	}*/
 }
 
 void getPosVelocityFeature(Mat &features, const Mat &timeMat, const Mat &posMat, const Mat &oriMat) {
@@ -78,29 +75,30 @@ void getPosVelocityFeature(Mat &features, const Mat &timeMat, const Mat &posMat,
 	for (int i = 0; i < features.size().height; ++i) {
 		features.row(i) = ((posMat.row(i + 1) - posMat.row(i)));// / (timeMat.at<float>(i + 1, 3) - timeMat.at<float>(i, 3)));
 	}
-	cout << "features: " << endl;
+	/*cout << "features: " << endl;
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < features.size().width; ++j) {
 			cout << features.at<float>(i, j) << " ";
 		}
 		cout << endl;
-	}
+	}*/
 }
 
 // Train the SVM model.
-void trainSVMModel(Mat &trainSet, Mat &trainLabels, CvSVM &svm) { // TODO: consider combining train and test into one method, so can encapsulate even the cvstatmodel used
+void trainSVMModel(Mat &trainSet, Mat &trainLabels, Mat &varIdx, CvSVM &svm) { // TODO: consider combining train and test into one method, so can encapsulate even the cvstatmodel used
 	// Set up SVM's parameters
 	CvSVMParams params;
 	params.svm_type = CvSVM::C_SVC;
 	params.kernel_type = CvSVM::RBF; //TODO: note this changes during testing with different vals!
 	params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
 
-	svm.train(trainSet, trainLabels, Mat(), Mat(), params);
-	//svm.train_auto(trainSet, trainLabels, Mat(), Mat(), params);
+	svm.train(trainSet, trainLabels, varIdx, Mat(), params);
+	//CvSVMParams params2;
+	//svm.train_auto(trainSet, trainLabels, varIdx, Mat(), params2);
 }
 
-void trainKNNModel(Mat &trainSet, Mat &trainLabels, CvKNearest &knn) {
-	knn.train(trainSet, trainLabels);
+void trainKNNModel(Mat &trainSet, Mat &trainLabels, Mat &varIdx, CvKNearest &knn) {
+	knn.train(trainSet, trainLabels, varIdx);
 }
 
 // Generates responses (predictions) for the test set and outputs results.
@@ -187,39 +185,73 @@ void seeAllMats(Mat trainTimeMat, Mat trainOriMat, Mat trainLabels, Mat trainFea
 	cout << "confusionMat: " << endl << confusionMat << endl << endl;
 }
 
+// Only the best joints.
+void getVarIdx(Mat &varIdx) {
+	int vars[] = {12, 13, 14, 15, //3
+				  16, 17, 18, 19, //4
+				  20, 21, 22, 23, //5
+				  32, 33, 34, 35, //8
+				  36, 37, 38, 39, //9
+				  40, 41, 42, 43, //10
+				  52, 53, 54, 55, //13
+				  56, 57, 58, 59, //14
+				  68, 69, 70, 71, //17
+				  72, 73, 74, 75};//18
+
+	Mat(4 * 10, 1, CV_32S, vars).copyTo(varIdx); //TODO: be careful using right number here...
+}
+
 // Get samples and labels from xml, then extract features,
 // shuffle and split the samples randomly intro training and test sets,
 // train the model, and predict outcomes from the test set, outputting classification rates.
 int main(int, char**) {
 	const String exerciseTypeName = String(getExerciseTypeName());
 
-	Mat trainTimeMat, trainPosMat, trainOriMat, trainLabels, trainFeatures;
+	Mat trainTimeMat, trainPosMat, trainOriMat, trainLabels, trainFeatures, trainFeatures1, trainFeatures2; //TODO: make these multiple features one, and inside the getFeature, concat the stuff.
 	getSamples('r', exerciseTypeName, trainTimeMat, trainPosMat, trainOriMat, trainLabels);
-	getRawOriFeature(trainFeatures, trainTimeMat, trainPosMat, trainOriMat);
-	//getRawPosFeature(trainFeatures, trainTimeMat, trainPosMat, trainOriMat);
+	getRawOriFeature(trainFeatures1, trainTimeMat, trainPosMat, trainOriMat);
+	getOriVelocityFeature(trainFeatures2, trainTimeMat, trainPosMat, trainOriMat); //TODO: should be an option
+	hconcat(trainFeatures1, trainFeatures2, trainFeatures); //TODO: should be an option
 
-	Mat testTimeMat, testPosMat, testOriMat, testLabels, testFeatures;
+	Mat testTimeMat, testPosMat, testOriMat, testLabels, testFeatures, testFeatures1, testFeatures2;
 	getSamples('t', exerciseTypeName, testTimeMat, testPosMat, testOriMat, testLabels);
-	getRawOriFeature(testFeatures, testTimeMat, testPosMat, testOriMat);
-	//getRawPosFeature(testFeatures, testTimeMat, testPosMat, testOriMat);
+	getRawOriFeature(testFeatures1, testTimeMat, testPosMat, testOriMat);
+	getOriVelocityFeature(testFeatures2, testTimeMat, testPosMat, testOriMat); //TODO: should be an option
+	hconcat(testFeatures1, testFeatures2, testFeatures); //TODO: should be an option
 
-	CvSVM svm;
-	trainSVMModel(trainFeatures, trainLabels, svm);
-
-	//TODO: make it an input which model to use (KNN, SVM, etc.)
-	//CvKNearest knn;
-	//trainKNNModel(trainFeatures, trainLabels, knn);
+	char feats = 'a';
+	std::cout << "Which features? 'a' all, 'l' less" << endl;
+	std::cin >> feats;
 
 	Mat responses;
-	//predictRandom(testFeatures, testLabels, responses);
-	predictSVM(testFeatures, testLabels, responses, svm);
-	//predictKNN(testFeatures, testLabels, responses, knn);
+	Mat varIdx;
+	if (feats == 'l') {
+		getVarIdx(varIdx); //TODO: make sure this can be controlled which vars, especially all of them if desired.
+	}
+
+	char modelType = 's';
+	std::cout << "What type of model? 's' SVM, 'k' KNN" << endl;
+	std::cin >> modelType;
+
+	if (modelType == 's') {
+		CvSVM svm;
+		trainSVMModel(trainFeatures, trainLabels, varIdx, svm);
+		predictSVM(testFeatures, testLabels, responses, svm);
+	}
+	else if (modelType == 'k') {
+		CvKNearest knn;
+		trainKNNModel(trainFeatures, trainLabels, varIdx, knn);
+		predictKNN(testFeatures, testLabels, responses, knn);
+	}
+	else {
+		predictRandom(testFeatures, testLabels, responses);
+	}
 
 	Mat confusionMat = Mat::zeros(4, 4, CV_32S);
 	aggregateResults(responses, testLabels, confusionMat);
 
-	//outputResults(confusionMat);
-	seeAllMats(trainTimeMat, trainOriMat, trainLabels, trainFeatures, testTimeMat, testOriMat, testLabels, testFeatures, responses, confusionMat);
+	outputResults(confusionMat);
+	//seeAllMats(trainTimeMat, trainOriMat, trainLabels, trainFeatures, testTimeMat, testOriMat, testLabels, testFeatures, responses, confusionMat);
 
 	char in;
 	cin >> in;
